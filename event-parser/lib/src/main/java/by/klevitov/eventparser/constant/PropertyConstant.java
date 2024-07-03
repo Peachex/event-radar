bdf46844f198638ee24027b561a08055805c03e5
Aleksey Klevitov <klevolex@gmail.com>
2024-07-03 17:52:19 +0300

    code refactoring

--- event-parser/lib/src/main/java/by/klevitov/eventparser/constant/PropertyConstant.java
+++ event-parser/lib/src/main/java/by/klevitov/eventparser/constant/PropertyConstant.java
@@ -3,9 +3,9 @@ package by.klevitov.eventparser.constant;
 public final class PropertyConstant {
     public static final String PROPERTY_FILE_WITH_SITES_FOR_PARSING = "sites_for_parsing.yml";
 
-    public static final String AFISHA_RELAX_SITE_URL = "afisha-relax-event-parser-site-url";
+    public static final String AFISHA_RELAX_SITE_URL = "afisha-relax-site-url";
 
-    public static final String BYCARD_SITE_URL = "bycard-event-parser-site-url";
+    public static final String BYCARD_SITE_URL = "bycard-site-url";
 
     private PropertyConstant() {
     }
