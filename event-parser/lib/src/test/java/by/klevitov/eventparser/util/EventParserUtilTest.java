4dcc563f65b726c313d82d5d071dba159f22443a
Aleksey Klevitov <klevolex@gmail.com>
2024-07-17 21:15:17 +0300

    add test

--- event-parser/lib/src/test/java/by/klevitov/eventparser/util/EventParserUtilTest.java
+++ event-parser/lib/src/test/java/by/klevitov/eventparser/util/EventParserUtilTest.java
@@ -5,7 +5,9 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.lang.reflect.Method;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
@@ -16,7 +18,9 @@ import static by.klevitov.eventparser.util.EventParserUtil.EventDateLocale.RUSSI
 import static by.klevitov.eventparser.util.EventParserUtil.convertStringToLocalDate;
 import static by.klevitov.eventparser.util.EventParserUtil.parseDateAndAddToMap;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class EventParserUtilTest {
     @ParameterizedTest
@@ -37,44 +41,94 @@ public class EventParserUtilTest {
     }
 
     @Test
-    public void test_convertDateToLocalString_withNullDateStrAndNullFormatter() {
-        Exception exception = assertThrows(DateConversionException.class, () -> convertStringToLocalDate(null, null));
+    public void test_convertStringToLocalDate_withNullDateStrAndNullFormatter() {
+        Exception exception = assertThrows(DateConversionException.class, () ->
+                convertStringToLocalDate(null, null));
         String expectedMessage = "Date cannot be null or empty: null";
         String actualMessage = exception.getMessage();
         assertEquals(expectedMessage, actualMessage);
     }
 
     @Test
-    public void test_convertDateToLocalString_withEmptyDateStrAndNullFormatter() {
-        Exception exception = assertThrows(DateConversionException.class, () -> convertStringToLocalDate("", null));
+    public void test_convertStringToLocalDate_withEmptyDateStrAndNullFormatter() {
+        Exception exception = assertThrows(DateConversionException.class, () ->
+                convertStringToLocalDate("", null));
         String expectedMessage = "Date cannot be null or empty: ";
         String actualMessage = exception.getMessage();
         assertEquals(expectedMessage, actualMessage);
     }
 
     @Test
-    public void test_convertDateToLocalString_withInvalidDateStrAndNullFormatter() {
-        Exception exception = assertThrows(DateConversionException.class, () -> convertStringToLocalDate("invalidDate",
-                null));
+    public void test_convertStringToLocalDate_withInvalidDateStrAndNullFormatter() {
+        Exception exception = assertThrows(DateConversionException.class, () ->
+                convertStringToLocalDate("invalidDate", null));
         String expectedMessage = "Date cannot be parsed from string: invalidDate 2024";
         String actualMessage = exception.getMessage();
         assertEquals(expectedMessage, actualMessage);
     }
 
     @Test
-    public void test_convertDateToLocalString_withValidDateStrAndNullFormatter() {
+    public void test_convertStringToLocalDate_withValidDateStrAndNullFormatter() {
         Pair<LocalDate, LocalDate> expected = Pair.of(LocalDate.of(LocalDate.now().getYear(), 6, 11), null);
         Pair<LocalDate, LocalDate> actual = convertStringToLocalDate("с 11 июня", null);
         assertEquals(expected, actual);
     }
 
     @Test
-    public void test_convertDateToLocalString_withValidDateStrAndValidFormatter() {
+    public void test_convertStringToLocalDate_withValidDateStrAndValidFormatter() {
         Pair<LocalDate, LocalDate> expected = Pair.of(null,
                 LocalDate.of(LocalDate.now().getYear(), 6, 11));
         Pair<LocalDate, LocalDate> actual = convertStringToLocalDate("только по 11 июня",
                 DateTimeFormatter.ofPattern("только по d MMMM yyyy", RUSSIAN.getLocale()));
         assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"11 июня", "с 11 июня", "по 11 июня", "до 11 июня"})
+    public void test_dateIsSingle_withSingleDate(String singleDateStr) throws Exception {
+        Method privateMethod = EventParserUtil.class.getDeclaredMethod("dateIsSingle", String.class);
+        privateMethod.setAccessible(true);
+        assertTrue((boolean) privateMethod.invoke(EventParserUtil.class, singleDateStr));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"11 июня - 15 июля", "c 11 июня - по 15 июля"})
+    public void test_dateIsSingle_withDualDate(String dualDateStr) throws Exception {
+        Method privateMethod = EventParserUtil.class.getDeclaredMethod("dateIsSingle", String.class);
+        privateMethod.setAccessible(true);
+        assertFalse((boolean) privateMethod.invoke(EventParserUtil.class, dualDateStr));
+    }
+
+    @ParameterizedTest
+    @MethodSource("datePairProvider")
+    public void test_parseLocalDate_withValidDateWithDefaultFormatter(Pair<LocalDate, String> arguments) throws Exception {
+        Method privateMethod = EventParserUtil.class.getDeclaredMethod("parseLocalDate", String.class,
+                DateTimeFormatter.class);
+        privateMethod.setAccessible(true);
+        LocalDate expected = arguments.getKey();
+        LocalDate actual = (LocalDate) privateMethod.invoke(EventParserUtil.class, arguments.getValue(),
+                EventParserUtil.DEFAULT_DATE_FORMATTER);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test_parseLocalDate_withValidDateWithCustomFormatter() throws Exception {
+        Method privateMethod = EventParserUtil.class.getDeclaredMethod("parseLocalDate", String.class,
+                DateTimeFormatter.class);
+        privateMethod.setAccessible(true);
+        LocalDate expected = LocalDate.of(LocalDate.now().getYear(), 12, 5);
+        LocalDate actual = (LocalDate) privateMethod.invoke(EventParserUtil.class, "только по 5 декабря",
+                DateTimeFormatter.ofPattern("только по d MMMM yyyy", RUSSIAN.getLocale()));
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Pair<LocalDate, String>> datePairProvider() {
+        return Stream.of(
+                Pair.of(LocalDate.of(LocalDate.now().getYear(), 6, 11), "11 июня"),
+                Pair.of(LocalDate.of(LocalDate.now().getYear(), 8, 23), "с 23 августа"),
+                Pair.of(LocalDate.of(LocalDate.now().getYear(), 10, 5), "по 5 октября"),
+                Pair.of(LocalDate.of(LocalDate.now().getYear(), 5, 30), "до 30 мая")
+        );
     }
 
     private static Stream<Pair<Map<String, String>, Map<String, String>>> pairProvider() {
@@ -102,10 +156,19 @@ public class EventParserUtilTest {
         mapWithNullStartDate.put("startDate", null);
         mapWithNullStartDate.put("endDate", LocalDate.now().getYear() + "-12-31");
 
+        Map<String, String> expectedMapWithNullStartDateAndExclusiveEndDate = new HashMap<>();
+        expectedMapWithNullStartDateAndExclusiveEndDate.put("dateStr", "до 31 декабря");
+
+        Map<String, String> mapWithNullStartDateAndExclusiveEndDate = new HashMap<>();
+        mapWithNullStartDateAndExclusiveEndDate.put("dateStr", "до 31 декабря");
+        mapWithNullStartDateAndExclusiveEndDate.put("startDate", null);
+        mapWithNullStartDateAndExclusiveEndDate.put("endDate", LocalDate.now().getYear() + "-12-31");
+
         return Stream.of(
                 Pair.of(expectedMapWithoutNullDates, mapWithoutNullDates),
                 Pair.of(expectedMapWithNullEndDate, mapWithNullEndDate),
-                Pair.of(expectedMapWithNullStartDate, mapWithNullStartDate)
+                Pair.of(expectedMapWithNullStartDate, mapWithNullStartDate),
+                Pair.of(expectedMapWithNullStartDateAndExclusiveEndDate, mapWithNullStartDateAndExclusiveEndDate)
         );
     }
 }
