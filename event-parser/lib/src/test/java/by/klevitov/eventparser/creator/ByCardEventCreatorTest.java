bb1309728bc4fa636e29fb35f60ff2b07b561ef7
Aleksey Klevitov <klevolex@gmail.com>
2024-07-20 22:01:37 +0300

    add test

--- event-parser/lib/src/test/java/by/klevitov/eventparser/creator/ByCardEventCreatorTest.java
+++ event-parser/lib/src/test/java/by/klevitov/eventparser/creator/ByCardEventCreatorTest.java
@@ -5,6 +5,7 @@ import by.klevitov.eventparser.util.EventCreatorUtil;
 import by.klevitov.eventradarcommon.dto.AbstractEventDTO;
 import by.klevitov.eventradarcommon.dto.ByCardEventDTO;
 import by.klevitov.eventradarcommon.dto.EventDate;
+import by.klevitov.eventradarcommon.dto.EventPrice;
 import by.klevitov.eventradarcommon.dto.EventSourceType;
 import by.klevitov.eventradarcommon.dto.Location;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
@@ -23,14 +25,18 @@ import static by.klevitov.eventparser.constant.EventField.EVENT_LINK;
 import static by.klevitov.eventparser.constant.EventField.IMAGE_LINK;
 import static by.klevitov.eventparser.constant.EventField.LOCATION_CITY;
 import static by.klevitov.eventparser.constant.EventField.LOCATION_COUNTRY;
+import static by.klevitov.eventparser.constant.EventField.MAX_PRICE;
+import static by.klevitov.eventparser.constant.EventField.MIN_PRICE;
 import static by.klevitov.eventparser.constant.EventField.PRICE_STR;
 import static by.klevitov.eventparser.constant.EventField.SOURCE_TYPE;
 import static by.klevitov.eventparser.constant.EventField.START_DATE;
 import static by.klevitov.eventparser.constant.EventField.TITLE;
+import static by.klevitov.eventparser.util.EventCreatorUtil.createEventDate;
+import static by.klevitov.eventparser.util.EventCreatorUtil.createEventPrice;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ByCardEventCreatorTest {
-    private static EventCreator eventCreator = new ByCardEventCreator();
+    private static final EventCreator eventCreator = new ByCardEventCreator();
     private static Map<String, String> fields;
 
     @BeforeEach
@@ -45,6 +51,8 @@ public class ByCardEventCreatorTest {
         fields.put(START_DATE, "startDate");
         fields.put(END_DATE, "endDate");
         fields.put(PRICE_STR, "priceStr");
+        fields.put(MIN_PRICE, "minPrice");
+        fields.put(MAX_PRICE, "maxPrice");
         fields.put(EVENT_LINK, "eventLink");
         fields.put(IMAGE_LINK, "imageLink");
     }
@@ -52,9 +60,12 @@ public class ByCardEventCreatorTest {
     @Test
     public void test_create() {
         try (MockedStatic<EventCreatorUtil> eventCreatorUtil = Mockito.mockStatic(EventCreatorUtil.class)) {
-            eventCreatorUtil.when(() -> EventCreatorUtil.createEventDate(Mockito.anyString(), Mockito.anyString()))
+            eventCreatorUtil.when(() -> createEventDate(Mockito.anyString(), Mockito.anyString()))
                     .thenReturn(new EventDate(LocalDate.of(2024, 7, 13),
                             LocalDate.of(2024, 7, 16)));
+            eventCreatorUtil.when(() -> createEventPrice(Mockito.anyString(), Mockito.anyString()))
+                    .thenReturn(new EventPrice(new BigDecimal("12.34"), new BigDecimal("567.89")));
+
             AbstractEventDTO expected = ByCardEventDTO.builder()
                     .title(fields.get(TITLE))
                     .location(new Location(fields.get(LOCATION_COUNTRY), fields.get(LOCATION_CITY)))
@@ -63,11 +74,13 @@ public class ByCardEventCreatorTest {
                     .dateStr(fields.get(DATE_STR))
                     .date(new EventDate(LocalDate.of(2024, 7, 13), LocalDate.of(2024, 7, 16)))
                     .priceStr(fields.get(PRICE_STR))
+                    .price(new EventPrice(new BigDecimal("12.34"), new BigDecimal("567.89")))
                     .eventLink(fields.get(EVENT_LINK))
                     .imageLink(fields.get(IMAGE_LINK))
                     .build();
+
             AbstractEventDTO actual = eventCreator.create(fields);
-            eventCreatorUtil.verify(() -> EventCreatorUtil.createEventDate(Mockito.anyString(), Mockito.anyString()));
+            eventCreatorUtil.verify(() -> createEventDate(Mockito.anyString(), Mockito.anyString()));
             assertEquals(expected, actual);
         }
     }
