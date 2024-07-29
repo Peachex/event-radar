c4e43acd5fd7e512f2f3ac916b4e8d9de4bdcd9c
Aleksey Klevitov <klevolex@gmail.com>
2024-07-30 00:16:30 +0300

    initial commit

---
+++ event-persistor/src/test/java/by/klevitov/eventpersistor/EventPersistorApplicationTests.java
@@ -0,0 +1,13 @@
+package by.klevitov.eventpersistor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPersistorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
