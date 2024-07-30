3e78d9585f3c1f37ec768f7fb1d1b08ff83674de
Aleksey Klevitov <klevolex@gmail.com>
2024-07-30 19:27:53 +0300

    setup logger

--- event-persistor/src/main/java/by/klevitov/eventpersistor/web/controller/HealthController.java
+++ event-persistor/src/main/java/by/klevitov/eventpersistor/web/controller/HealthController.java
@@ -1,12 +1,10 @@
 package by.klevitov.eventpersistor.web.controller;
 
-import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Log4j2
 public class HealthController {
     private final String HEALTH_KEY = "status";
     private final String HEALTH_VALUE = "UP.";
@@ -16,13 +14,9 @@ public class HealthController {
         return Pair.of(HEALTH_KEY, HEALTH_VALUE);
     }
 
-    //fixme Fix issue with logger - write logs to console and file + keep original spring console logs.
-
     @GetMapping("/resourcecheck")
     public Pair<String, String> checkResource() {
         //todo Add resource check - check if database/message queue is available.
-        log.warn("Null response!");
-        log.info("Info 123");
         return null;
     }
 }
