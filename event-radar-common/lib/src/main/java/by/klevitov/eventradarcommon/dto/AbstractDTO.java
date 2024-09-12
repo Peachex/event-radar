563bd8b33daf623677046ef02e8971dfbb7fcdd6
Aleksey Klevitov <klevolex@gmail.com>
2024-09-12 23:02:19 +0300

    finally fix the issue with objects comparing

--- event-radar-common/lib/src/main/java/by/klevitov/eventradarcommon/dto/AbstractDTO.java
+++ event-radar-common/lib/src/main/java/by/klevitov/eventradarcommon/dto/AbstractDTO.java
@@ -1,8 +1,10 @@
 package by.klevitov.eventradarcommon.dto;
 
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+@Data
 @NoArgsConstructor
 @SuperBuilder
 public abstract class AbstractDTO {
