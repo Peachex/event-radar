3f1493a5acfb2c480e672fcbf45875e516587657
Aleksey Klevitov <klevolex@gmail.com>
2024-09-12 22:35:29 +0300

    fix compile issue

--- event-radar-common/lib/src/main/java/by/klevitov/eventradarcommon/dto/AbstractEventDTO.java
+++ event-radar-common/lib/src/main/java/by/klevitov/eventradarcommon/dto/AbstractEventDTO.java
@@ -1,6 +1,7 @@
 package by.klevitov.eventradarcommon.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
