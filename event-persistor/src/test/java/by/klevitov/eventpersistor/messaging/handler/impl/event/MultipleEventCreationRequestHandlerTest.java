57c4c1e7cc9f89446c02e9cf2c9708affbd73c11
Aleksey Klevitov <klevolex@gmail.com>
2024-10-12 21:17:27 +0300

    add test

--- event-persistor/src/test/java/by/klevitov/eventpersistor/messaging/handler/impl/event/MultipleEventCreationRequestHandlerTest.java
+++ event-persistor/src/test/java/by/klevitov/eventpersistor/messaging/handler/impl/event/MultipleEventCreationRequestHandlerTest.java
@@ -1,33 +1,38 @@
 package by.klevitov.eventpersistor.messaging.handler.impl.event;
 
 import by.klevitov.eventpersistor.messaging.exception.RequestHandlerException;
+import by.klevitov.eventpersistor.messaging.factory.EntityConverterFactory;
 import by.klevitov.eventpersistor.messaging.handler.RequestHandler;
 import by.klevitov.eventpersistor.persistor.service.EventService;
 import by.klevitov.eventradarcommon.messaging.request.EntityData;
-import by.klevitov.eventradarcommon.messaging.request.data.EntityIdData;
+import by.klevitov.eventradarcommon.messaging.request.data.MultipleEventData;
 import by.klevitov.eventradarcommon.messaging.response.MessageResponse;
 import by.klevitov.eventradarcommon.messaging.response.SuccessfulMessageResponse;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.util.ArrayList;
+
 import static by.klevitov.eventpersistor.messaging.handler.HandlerUtil.assertEqualsExcludingCreatedDate;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class DeletionEventRequestHandlerTest {
+public class MultipleEventCreationRequestHandlerTest {
     private static RequestHandler handler;
     private static EventService eventService;
+    private static EntityConverterFactory converterFactory;
 
     @BeforeAll
     public static void init() {
         eventService = Mockito.mock(EventService.class);
-        handler = new DeletionEventRequestHandler(eventService);
+        converterFactory = Mockito.mock(EntityConverterFactory.class);
+        handler = new MultipleEventCreationRequestHandler(eventService, converterFactory);
     }
 
     @Test
     public void test_handle_withValidEntityData() {
-        EntityData entityData = new EntityIdData("id");
-        MessageResponse expected = new SuccessfulMessageResponse(false);
+        EntityData entityData = new MultipleEventData(new ArrayList<>());
+        MessageResponse expected = new SuccessfulMessageResponse(new MultipleEventData(new ArrayList<>()));
         MessageResponse actual = handler.handle(entityData);
         assertEqualsExcludingCreatedDate(expected, actual);
     }
