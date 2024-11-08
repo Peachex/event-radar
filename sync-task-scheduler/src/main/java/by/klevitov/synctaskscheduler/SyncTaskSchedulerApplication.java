2d854457c25affd352b09922a4f2cff12c803fd1
Aleksey Klevitov <klevolex@gmail.com>
2024-11-08 10:37:14 +0300

    create project

---
+++ sync-task-scheduler/src/main/java/by/klevitov/synctaskscheduler/SyncTaskSchedulerApplication.java
@@ -0,0 +1,12 @@
+package by.klevitov.synctaskscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncTaskSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyncTaskSchedulerApplication.class, args);
+    }
+}
---
+++ sync-task-scheduler/src/test/java/by/klevitov/synctaskscheduler/SyncTaskSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.klevitov.synctaskscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncTaskSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
