c4c472ffd34889b5b8c29311ef182aec79a7b698
Aleksey Klevitov <klevolex@gmail.com>
2025-01-03 20:55:14 +0300

    add test and disable logging for tests

---
+++ event-manager/src/test/java/by/klevitov/eventmanager/manager/util/EventPersistorClientUtilTest.java
@@ -0,0 +1,30 @@
+package by.klevitov.eventmanager.manager.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class EventPersistorClientUtilTest {
+    private EventPersistorClientUtil clientUtil;
+    private ObjectMapper mockedObjectMapper;
+
+    @BeforeEach
+    public void setUp() {
+        mockedObjectMapper = Mockito.mock(ObjectMapper.class);
+        clientUtil = new EventPersistorClientUtil(mockedObjectMapper);
+    }
+
+    @Test
+    public void test_extractResponseFromBody() throws IOException {
+        InputStream body = InputStream.nullInputStream();
+        clientUtil.extractResponseFromBody(body, Object.class);
+        verify(mockedObjectMapper, times(1)).readValue(body, Object.class);
+    }
+}
