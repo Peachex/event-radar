0e779a654170a1724e3789c0502b859395689f69
Aleksey Klevitov <klevolex@gmail.com>
2025-01-08 00:01:10 +0300

    add swagger

--- sync-task-scheduler/src/main/java/by/klevitov/synctaskscheduler/SyncTaskSchedulerApplication.java
+++ sync-task-scheduler/src/main/java/by/klevitov/synctaskscheduler/SyncTaskSchedulerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SyncTaskSchedulerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SyncTaskSchedulerApplication.class, args);
     }
